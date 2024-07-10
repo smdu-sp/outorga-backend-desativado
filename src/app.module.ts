a1e890d45c1e0f27a5f8551c44f282fe8cb6a831
Victor Abreu <44484263+aalevictor@users.noreply.github.com>
2024-07-10 14:16:32 -0300

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Global, Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { RoleGuard } from './auth/guards/role.guard';
+import { PrismaModule } from './prisma/prisma.module';
+import { Prisma2Module } from './prisma2/prisma2.module';
+
+@Global()
+@Module({
+  imports: [UsuariosModule, AuthModule, PrismaModule, Prisma2Module],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard,
+    },
+  ],
+  exports: [AppService],
+})
+export class AppModule {}
